e19005e8c792e72a383182e1f9f51d90232c2076
Kshitij Saraogi <kshitij.saraogi@gmail.com>
2019-09-19 16:29:52 +0530

    [metricbeat/mongodb]: Add command field to schemas

--- metricbeat/module/mongodb/currentops/data.go
+++ metricbeat/module/mongodb/currentops/data.go
@@ -23,6 +23,30 @@ var schemaNone = s.Schema{
 	"waiting_for_locks": c.Bool("waitingForLock", s.Optional),
 }
 
+var schemacommand = s.Schema{
+	"truncated":  c.Str("$truncated", s.Optional),
+	"comment":    c.Str("comment", s.Optional),
+	"db":         c.Str("$db", s.Optional),
+	"get_more":   c.Int("getMore", s.Optional),
+	"collection": c.Str("collection", s.Optional),
+	"batch_size": c.Int("batchSize", s.Optional),
+	"term":       c.Int("term", s.Optional),
+	"find":       c.Str("find", s.Optional),
+	"filter": c.DictOptional(c.Dict("filter", s.Schema{
+		"ts": c.DictOptional(c.Dict("ts", s.Schema{
+			"gte": c.Str("$gte", s.Optional),
+		})),
+	})),
+	"tailable":     c.Bool("tailable", s.Optional),
+	"oplog_replay": c.Bool("oplogReplay", s.Optional),
+	"await_data":   c.Bool("awaitData", s.Optional),
+	"aggregate":    c.Int("aggregate", s.Optional),
+	"from_mongos":  c.Bool("fromMongos", s.Optional),
+	"cursor": c.DictOptional(c.Dict("cursor", s.Schema{
+		"batch_size": c.Int("batchSize", s.Optional),
+	})),
+}
+
 var schemaCommand = s.Schema{
 	"shard":         c.Str("shard", s.Optional),
 	"host":          c.Str("host", s.Optional),
@@ -38,7 +62,7 @@ var schemaCommand = s.Schema{
 	"microsecs_running": c.Int("microsecs_running", s.Optional),
 	"op":                c.Str("op", s.Optional),
 	"ns":                c.Str("ns", s.Optional),
-	// TODO: Add command
+	"command":           c.DictOptional(c.Dict("command", schemacommand)),
 	"num_yields":        c.Int("numYields", s.Optional),
 	"waiting_for_locks": c.Bool("waitingForLock", s.Optional),
 }
@@ -52,21 +76,76 @@ var schemaGetMore = s.Schema{
 	// TODO: Add client metadata
 	"active": c.Bool("active"),
 	// TODO: Parse this as time
+	"current_op_time":     c.Str("currentOpTime", s.Optional),
+	"opid":                c.Str("opid", s.Optional),
+	"secs_running":        c.Int("secs_running", s.Optional),
+	"microsecs_running":   c.Int("microsecs_running", s.Optional),
+	"op":                  c.Str("op", s.Optional),
+	"ns":                  c.Str("ns", s.Optional),
+	"command":             c.DictOptional(c.Dict("command", schemacommand)),
+	"originating_command": c.DictOptional(c.Dict("originatingCommand", schemacommand)),
+	"plan_summary":        c.Str("planSummary", s.Optional),
+	"num_yields":          c.Int("numYields", s.Optional),
+	"waiting_for_locks":   c.Bool("waitingForLock", s.Optional),
+}
+
+var schemaUpdate = s.Schema{
+	"shard":             c.Str("shard", s.Optional),
+	"host":              c.Str("host", s.Optional),
+	"desc":              c.Str("desc", s.Optional),
+	"active":            c.Bool("active", s.Optional),
 	"current_op_time":   c.Str("currentOpTime", s.Optional),
-	"opid":              c.Int("opid", s.Optional),
-	"secs_running":      c.Int("secs_running", s.Optional),
-	"microsecs_running": c.Int("microsecs_running", s.Optional),
+	"opid":              c.Str("opid", s.Optional),
 	"op":                c.Str("op", s.Optional),
 	"ns":                c.Str("ns", s.Optional),
-	// TODO: Add command
-	// TODO: Add originating command
-	"plan_summary":      c.Str("planSummary", s.Optional),
 	"num_yields":        c.Int("numYields", s.Optional),
 	"waiting_for_locks": c.Bool("waitingForLock", s.Optional),
 }
-
-var schemaUpdate = s.Schema{}
-var schemaInsert = s.Schema{}
-var schemaQuery = s.Schema{}
-var schemaRemove = s.Schema{}
-var schemaKillCursors = s.Schema{}
+var schemaInsert = s.Schema{
+	"shard":             c.Str("shard", s.Optional),
+	"host":              c.Str("host", s.Optional),
+	"desc":              c.Str("desc", s.Optional),
+	"active":            c.Bool("active", s.Optional),
+	"current_op_time":   c.Str("currentOpTime", s.Optional),
+	"opid":              c.Str("opid", s.Optional),
+	"op":                c.Str("op", s.Optional),
+	"ns":                c.Str("ns", s.Optional),
+	"num_yields":        c.Int("numYields", s.Optional),
+	"waiting_for_locks": c.Bool("waitingForLock", s.Optional),
+}
+var schemaQuery = s.Schema{
+	"shard":             c.Str("shard", s.Optional),
+	"host":              c.Str("host", s.Optional),
+	"desc":              c.Str("desc", s.Optional),
+	"active":            c.Bool("active", s.Optional),
+	"current_op_time":   c.Str("currentOpTime", s.Optional),
+	"opid":              c.Str("opid", s.Optional),
+	"op":                c.Str("op", s.Optional),
+	"ns":                c.Str("ns", s.Optional),
+	"num_yields":        c.Int("numYields", s.Optional),
+	"waiting_for_locks": c.Bool("waitingForLock", s.Optional),
+}
+var schemaRemove = s.Schema{
+	"shard":             c.Str("shard", s.Optional),
+	"host":              c.Str("host", s.Optional),
+	"desc":              c.Str("desc", s.Optional),
+	"active":            c.Bool("active", s.Optional),
+	"current_op_time":   c.Str("currentOpTime", s.Optional),
+	"opid":              c.Str("opid", s.Optional),
+	"op":                c.Str("op", s.Optional),
+	"ns":                c.Str("ns", s.Optional),
+	"num_yields":        c.Int("numYields", s.Optional),
+	"waiting_for_locks": c.Bool("waitingForLock", s.Optional),
+}
+var schemaKillCursors = s.Schema{
+	"shard":             c.Str("shard", s.Optional),
+	"host":              c.Str("host", s.Optional),
+	"desc":              c.Str("desc", s.Optional),
+	"active":            c.Bool("active", s.Optional),
+	"current_op_time":   c.Str("currentOpTime", s.Optional),
+	"opid":              c.Str("opid", s.Optional),
+	"op":                c.Str("op", s.Optional),
+	"ns":                c.Str("ns", s.Optional),
+	"num_yields":        c.Int("numYields", s.Optional),
+	"waiting_for_locks": c.Bool("waitingForLock", s.Optional),
+}
